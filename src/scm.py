a0986b2e014fc85b0fa7d0082d1de9eaa6fb5e66
hanita <hhaller@wisc.edu>
2025-03-31 15:03:10 -0400

    initial commit

---
+++ src/scm.py
@@ -0,0 +1,7 @@
+
+
+class SCM():
+    def __init__(self, v, f, u):
+        self.v = v
+        self.f = f
+        self.u = u 
---
+++ src/test.py
@@ -0,0 +1,6 @@
+
+
+def helper(str):
+    print("CI2-FAIRNESS-NCM test.py helper")
+    print(str)
+    print()
