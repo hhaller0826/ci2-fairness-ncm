b95022133dfc1207834daf34e6b071c3caf10c6e
hanita <hhaller@wisc.edu>
2026-03-24 19:55:00 -0400

    oops

--- src/model/distribution.py
+++ src/model/distribution.py
@@ -41,39 +41,39 @@ class UniformDistribution(Distribution):
         return u_vals
     
 
-# class NeuralDistribution(Distribution):
-#     def __init__(self, u_names, sizes, hyperparams, default_module=MLP, seed=None):
-#         assert set(sizes.keys()).issubset(set(u_names))
+class NeuralDistribution(Distribution):
+    def __init__(self, u_names, sizes, hyperparams, default_module=MLP, seed=None):
+        assert set(sizes.keys()).issubset(set(u_names))
 
-#         super().__init__(list(u_names))
-#         self.sizes = {U: sizes[U] if U in sizes else 1 for U in u_names}
-#         if seed is not None:
-#             self.rand_state = np.random.RandomState(seed=seed)
-#         else:
-#             self.rand_state = np.random.RandomState()
+        super().__init__(list(u_names))
+        self.sizes = {U: sizes[U] if U in sizes else 1 for U in u_names}
+        if seed is not None:
+            self.rand_state = np.random.RandomState(seed=seed)
+        else:
+            self.rand_state = np.random.RandomState()
 
-#         self.func = T.nn.ModuleDict({
-#             str(u): default_module(
-#                 {},
-#                 {u: self.sizes[u]},
-#                 self.sizes[u],
-#                 h_layers=hyperparams.get('h-layers', 2),
-#                 h_size=hyperparams.get('h-size', 128),
-#                 use_layer_norm=hyperparams.get('layer-norm', False),
-#                 use_sigmoid=False
-#             )
-#             for u in self.sizes})
+        self.func = T.nn.ModuleDict({
+            str(u): default_module(
+                {},
+                {u: self.sizes[u]},
+                self.sizes[u],
+                h_layers=hyperparams.get('h-layers', 2),
+                h_size=hyperparams.get('h-size', 128),
+                use_layer_norm=hyperparams.get('layer-norm', False),
+                use_sigmoid=False
+            )
+            for u in self.sizes})
 
-#     def sample(self, n=1, device=None):
-#         if device is None:
-#             device = self.device_param.device
+    def sample(self, n=1, device=None):
+        if device is None:
+            device = self.device_param.device
 
-#         u_vals = dict()
-#         for U in self.sizes:
-#             noise = T.randn((n, self.sizes[U])).float().to(device)
-#             u_vals[U] = self.func[str(U)]({}, {U: noise})
+        u_vals = dict()
+        for U in self.sizes:
+            noise = T.randn((n, self.sizes[U])).float().to(device)
+            u_vals[U] = self.func[str(U)]({}, {U: noise})
 
-#         return u_vals
+        return u_vals
 
 
 class BernoulliDistribution(Distribution):
